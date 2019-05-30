ae006675a1de390af212451b5f27f97cd4776488
Hitesh Nalamwar <hj.nalamwar@gmail.com>
2019-05-30 22:51:43 +0700

    SignIN screen UI

--- InstagramV1/Controller/LoginVC.swift
+++ InstagramV1/Controller/LoginVC.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class LoginVC: UIViewController {
     
+    let logoContainerView: UIView = {
+     
+        let view = UIView()
+        
+        let logoImageView = UIImageView(image: UIImage(named: "Instagram_logo_white"))
+        logoImageView.contentMode = .scaleAspectFill
+        view.addSubview(logoImageView)
+        logoImageView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 200, height: 50)
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        view.backgroundColor = UIColor(red: 0/255, green: 120/255, blue: 175/275, alpha: 1)
+        
+        return view
+    }()
+    
     let emailTextField : UITextField = {
         let tf = UITextField()
         tf.placeholder = "Enter your Email"
@@ -38,6 +53,18 @@ class LoginVC: UIViewController {
         return button
     }()
     
+    let signUpButton : UIButton = {
+        
+        let button = UIButton(type: .system)
+        
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an Account?  ",attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor:UIColor.lightGray])
+        
+        attributedTitle.append(NSAttributedString(string: "SignUp", attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)]))
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,32 +72,27 @@ class LoginVC: UIViewController {
         
         view.backgroundColor = .white
         
-        view.addSubview(emailTextField)
-        emailTextField.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 80, paddingBottom: 0, paddingLeft: 10, paddingRight: 10, width: 0, height: 0)
-
-        // Do any additional setup after loading the view.
+        view.addSubview(logoContainerView)
+        
+        logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 0, height: 150)
+        
+        configureViewComponents()
+        view.addSubview(signUpButton)
+        
+        signUpButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingBottom: 30, paddingLeft: 0, paddingRight: 0, width: 0, height: 50)
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureViewComponents() {
+        
+        let stackview = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
+        stackview.axis = .vertical
+        stackview.spacing = 10
+        stackview.distribution = .fillEqually
+        
+        view.addSubview(stackview)
+        stackview.anchor(top: logoContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingBottom: 0, paddingLeft: 40, paddingRight: 40, width: 0, height: 140)
+        
     }
-    */
+    
 
 }
